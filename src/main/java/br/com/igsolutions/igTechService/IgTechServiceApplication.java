a8563fdac0e8fee5ad186b685b7baeb6d84f663c
igorgp06 <igorgoncalvesp20@gmail.com>
2025-03-22 00:52:35 -0300

    [DEPLOY 1.0] Deploy inicial do projeto

---
+++ src/main/java/br/com/igsolutions/igTechService/IgTechServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.igsolutions.igTechService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgTechServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgTechServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/igsolutions/igTechService/IgTechServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igsolutions.igTechService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgTechServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
